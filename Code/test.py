9a2b174d502969e9e52297f735e3092952c74c08
redrubycorn <redrubycorn@gmail.com>
2024-10-04 23:32:53 +0200

    add code for servos

---
+++ Code/test.py
@@ -0,0 +1,13 @@
+from pca9685 import PCA9685
+from machine import I2C, Pin
+from servo import Servo
+
+sda = Pin(0)
+scl = Pin(1)
+id = 0
+i2c = I2C(id=id, sda=sda, scl=scl)
+
+pca = PCA9685(i2c=i2c)
+servo = Servo(i2c=i2c)
+
+servo.position(index=0, degrees=180)
